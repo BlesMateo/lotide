b9393d634c91dd731ecb7abab38cf27261d4ef4f
BlesMateo <blesildaangelicamateo@gmail.com>
2021-10-26 20:11:36 -0400

    Implement a function which takes 2 arrays and returns true or false

---
+++ eqArrays.js
@@ -0,0 +1,10 @@
+const eqArrays = function(Array, Array2) {
+if (Array.length !== Array2.length)
+return false;
+for (let i = 0; i < Array.length; i = i + 1) {
+  if (Array[i] !== Array2[i]) {
+    return false
+  }
+}
+return true
+}
