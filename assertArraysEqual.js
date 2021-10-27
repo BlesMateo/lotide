e2e1b047fab0430d5c350226fb61ca75e980b995
Bles Mateo <89666942+BlesMateo@users.noreply.github.com>
2021-10-27 08:49:31 -0400

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,16 @@
 const eqArrays = function(Array, Array2) {
-  if (Array.length !== Array2.length) {
-  console.log(`🛑🛑🛑 Assertion Failed: ${Array} !== ${Array2}.`);
-}
-  for (let i = 0; i < Array.length; i = i + 1) {
-    if (Array[i] !== Array2[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${Array} !== ${Array2}.`);
-    }
+if (Array.length !== Array2.length)
+return false;
+for (let i = 0; i < Array.length; i = i + 1) {
+  if (Array[i] !== Array2[i]) {
+    return false
   }
-  console.log(`✅✅✅ Assertion Passed: ${Array} === ${Array2}.`); }
+}
+return true
+}
+
+const assertArrayEqual = function(Array, Array2) {
+if (eqArrays(Array, Array2) !== false) 
+console.log(`🛑🛑🛑 Assertion Failed: ${Array} !== ${Array2}.`);
+
+else console.log(`✅✅✅ Assertion Passed: ${Array} === ${Array2}.`); }
