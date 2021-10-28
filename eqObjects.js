ced91d1abe3123c50d24886eff10ace91a2ac456
BlesMateo <blesildaangelicamateo@gmail.com>
2021-10-27 21:28:40 -0400

    Create a function that will take in two objects and return a perfect match that is true or false

---
+++ eqObjects.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const argument1 = Object.keys(object1);
+  const argument2 = Object.keys(object2);
+  if (argument1.length !== argument2.length) {
+    return false;
+  }
+  for (const key of argument1) {
+    const argument1Value = argument1[key];
+    const argument2Value = argument2[key];
+    if (Array.isArray(argument1Value) || Array.isArray(argument2Value)) {
+      if (!eqArrays(argument1Value, argument2Value)) {
+        return false;
+      }
+    } else if (argument1Value !== argument2Value) {
+      return false;
+    }
+  }
+  return true;
+  }
+
+  //Test Code:
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
