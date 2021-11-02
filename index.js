dca6c7cc413f4bbef212dd89196fd4f8cd4efc9d
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-01 21:45:04 -0400

    Exporting all current functions to lotide

---
+++ index.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyValue = require('./findKeyValue');
+const head   = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const moduleCheck = require('./moduleCheck');
+const myModule = require('./myModule');
+const tail   = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  myModule,
+  tail,
+  takeUntil,
+  without,
+};
