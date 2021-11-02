931f8e08fe5b05ebf42ce5873324c5737b3d972f
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-01 20:19:48 -0400

    Used chai to refactor the file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+describe("#tail", () => {
+
+  it("returns [5, 6] for [4 ,5 ,6 ]", () => {
+    assert.deepEqual (tail([4,5,6]), [5,6]);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2 ,3 ,4]), [2, 3, 4]);
+  });
+
+});
+
